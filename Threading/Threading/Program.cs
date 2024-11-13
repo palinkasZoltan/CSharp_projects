2e62c80d0f4c21f180fef05ec238ecc33ebc53cf
palinkasZoltan <77148226+palinkasZoltan@users.noreply.github.com>
2024-11-13 11:46:04 +0100

    Add full-basic threading "project"

---
+++ Threading/Threading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread workerThread = new Thread(WriteOnNewThread);
+            workerThread.Name = "Worker Thread";
+            workerThread.Start();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Main Thread: {0}", i);
+                Thread.Sleep(100);
+            }
+        }
+
+        private static void WriteOnNewThread()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Worker Thread: {0}", i);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
